776b3bdba51798a07189293d6b2a6b0380f6a004
MaximGromak <maximgromak@gmail.com>
2022-05-04 20:47:20 +1000

    widget impl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import HomePage from "./components/pages/HomePage";
+import DriftPage from "./components/pages/DriftPage";
+import TimeAttackPage from "./components/pages/TimeAttackPage";
+import ForzaPage from "./components/pages/ForzaPage";
+import Menu from "./components/Menu";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import '../src/css/main.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Menu/>
+            <div className="page">
+                <Routes>
+                    <Route path="/" exact element={<HomePage/>}/> // In V6, you can't use the 'component' prop anymore. It was replaced in favor of element :(
+                    <Route path="/drift" element={<DriftPage/>}/>
+                    <Route path="/timeattack" element={<TimeAttackPage/>}/>
+                    <Route path="/forza" element={<ForzaPage/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
