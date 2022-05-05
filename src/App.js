da1adafb7cb7b810855fcf5fbc2654c684e43d79
MaximGromak <maximgromak@gmail.com>
2022-05-06 00:09:41 +1000

    upd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
 import React from "react";
-import HomePage from "./components/pages/HomePage";
-import DriftPage from "./components/pages/DriftPage";
-import TimeAttackPage from "./components/pages/TimeAttackPage";
-import ForzaPage from "./components/pages/ForzaPage";
 import Menu from "./components/Menu";
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import '../src/css/main.css';
 
 function App() { // In V6, you can't use the 'component' prop anymore.It was replaced in favor of element :(
-    console.log(process.env.PUBLIC_URL)
     return (
         <Router>
             <Menu/>
-            <div className="page">
-                <Routes>
-                    <Route path="" exact element={<HomePage/>}/>
-                    <Route path="drift" element={<DriftPage/>}/>
-                    <Route path="timeattack" element={<TimeAttackPage/>}/>
-                    <Route path="forza" element={<ForzaPage/>}/>
-                </Routes>
-            </div>
         </Router>
     );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,29 @@
 import React from "react";
+import {Link, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TimeAttackPage from "./pages/TimeAttackPage";
+import ForzaPage from "./pages/ForzaPage";
+import DriftPage from "./pages/DriftPage";
 
 export default function Menu(props) {
     return (
-        <nav className="menu">
-            <a className="menu__item" href="/">Главная</a>
-            <a className="menu__item" href="/drift">Дрифт-такси</a>
-            <a className="menu__item" href="/timeattack">Time Attack</a>
-            <a className="menu__item" href="/forza">Forza Karting</a>
-        </nav>
+        <>
+            <nav className="menu">
+                <Link className="menu__item" to='/'> Главная </Link>
+                <Link className="menu__item" to='/drift'> Дрифт-такси </Link>
+                <Link className="menu__item" to='/timeattack'>Time Attack</Link>
+                <Link className="menu__item" to='/forza'> Forza Karting </Link>
+            </nav>
+
+            <Routes>
+                <Route exact path='/' element={<HomePage/>}/>
+
+                <Route exact path='/drift' element={<DriftPage/>}/>
+
+                <Route exact path='/timeattack' element={<TimeAttackPage/>}/>
+
+                <Route exact path='/forza' element={<ForzaPage/>}/>
+            </Routes>
+        </>
     );
 }
